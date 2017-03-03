e19b87e82af0610243a42cf4b191836a724a1f44
Richard Bowman <rwb34@bath.ac.uk>
2017-03-03 16:47:03 +0000

    Added a first very rough build script for the body only.

--- rebuild.py
+++ rebuild.py
@@ -11,7 +11,45 @@ import os, sys, shutil
 
 build_dir = "./builds"
 
-configurations = {}
+configurations = {
+                'common':{
+                    'constants':{},
+                    'build_files':['feet',
+                                   'gears',
+                                   'actuator_assembly_tools',
+                                   'sample_clips',
+                                   'small_gear',
+                                   ]
+                    },
+                'SS':{
+                    'constants':{'big_stage':'false',
+                                 'motor_lugs':'false'},
+                    'build_files':['main_body',
+                                   'illumination_and_back_foot'
+                                   ]
+                    },
+                'SS-M':{
+                    'constants':{'big_stage':'false',
+                                 'motor_lugs':'true'},
+                    'build_files':['main_body',
+                                   'illumination_and_back_foot',
+                                   ]
+                    },
+                'LS65':{
+                    'constants':{'big_stage':'true',
+                                 'motor_lugs':'false'},
+                    'build_files':['main_body',
+                                   'illumination_and_back_foot'
+                                   ]
+                    },
+                'LS65-M':{
+                    'constants':{'big_stage':'true',
+                                 'motor_lugs':'true'},
+                    'build_files':['main_body',
+                                   'illumination_and_back_foot'
+                                   ]
+                    },
+                 }
 
 # Start by cleaning out the build directory and making new folders.
 # I should probably be smarter about this...
