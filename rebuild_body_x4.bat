8dec9825c0f05ac7f8594c1ac26a3e56bd162d38
Richard Bowman <rwb34@bath.ac.uk>
2017-03-17 12:30:05 +0000

    rebuilt

--- rebuild_body_x4.bat
+++ rebuild_body_x4.bat
@@ -7,7 +7,7 @@ rem "This batch file recompiles the four standard versions of the microscope."
 
 rem "We now also generate illumination modules"
 
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_adj_SS.stl -D condenser=false -D big_stage=false OpenSCAD/illumination_and_back_foot.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_adj_LS65.stl -D condenser=false -D big_stage=true -D sample_z=65 OpenSCAD/illumination_and_back_foot.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_condenser_LS65.stl -D condenser=true -D big_stage=true -D sample_z=65 OpenSCAD/illumination_and_back_foot.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_condenser_LS75.stl -D condenser=true -D big_stage=true -D sample_z=75 OpenSCAD/illumination_and_back_foot.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_adj_SS.stl -D condenser=false -D big_stage=false OpenSCAD/illumination_and_rear_foot.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_adj_LS65.stl -D condenser=false -D big_stage=true -D sample_z=65 OpenSCAD/illumination_and_rear_foot.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_condenser_LS65.stl -D condenser=true -D big_stage=true -D sample_z=65 OpenSCAD/illumination_and_rear_foot.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o builds/illumination_and_back_foot_condenser_LS75.stl -D condenser=true -D big_stage=true -D sample_z=75 OpenSCAD/illumination_and_rear_foot.scad
