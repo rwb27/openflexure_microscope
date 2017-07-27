c2e74042db5c61aca643e52fbc93365b7f2bb0f1
Richard Bowman <richard.bowman@cantab.net>
2017-07-27 10:27:29 +0100

    Rebuilt everything
    Rebuilt all the STL files using the shiny new Makefile :)

--- rebuild_accessories.bat
+++
@@ -1,9 +0,0 @@
-rem "compiling feet, gears, and sample clips"
-
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/feet.stl  OpenSCAD/feet.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/feet_tall.stl -D foot_height=26  OpenSCAD/feet.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/gears.stl OpenSCAD/gears.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/sample_clips.stl OpenSCAD/sample_clips.scad
-
-rem "compiling elastic band tools"
-"C:\Program Files\OpenSCAD\openscad.com" -o builds/actuator_assembly_tools.stl OpenSCAD/actuator_assembly_tools.scad
